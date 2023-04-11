26d5d0a7cc3b5d05c4c3c8e52c342cd0e5120329
Ali Bayat <ali.bayat77@yahoo.com>
2023-04-11 21:49:48 +0430

    buttons and it's animation added

--- src/components/Landing ui/AuthenticatedHome.tsx
+++ src/components/Landing ui/AuthenticatedHome.tsx
@@ -1,19 +1,33 @@
 import cartShopLogo from './assets/CartShop-logo.png'
+import { useState } from 'react'
 
 const AuthenticatedHome = () => {
+  const [displaySection, setDisplaySection] = useState<string>('')
+
   return (
     <div className="flex flex-col justify-center items-center w-full">
       <div className="flex flex-row justify-between items-center fixed top-0 py-4 w-full h-[152px] bg-primary z-10">
         <div className="ml-4 sm:ml-16 bg-white w-[366px] h-12 rounded-lg mt-36 border border-silver relative">
           <button
-            className="button-sm w-[50%] h-full text-silver border-r border-r-silver border-dashed "
+            onClick={() => setDisplaySection('contact-us')}
+            className={`button-sm w-[50%] transition-all duration-300 h-full ${
+              displaySection === 'contact-us' ? 'text-primary' : 'text-silver'
+            }  border-r border-r-silver border-dashed`}
           >
             تماس با ما
           </button>
-          <button className="button-sm h-full w-[50%] text-primary">محصولات</button>
+          <button
+            onClick={() => setDisplaySection('products')}
+            className={`button-sm transition-all duration-300 h-full w-[50%] ${
+              displaySection === 'products' ? 'text-primary' : 'text-silver'
+            }`}
+          >
+            محصولات
+          </button>
           <div
-            style={{ borderRadius: '8px 8px 0 0' }}
-            className="w-[148px] h-[5px] bg-primary absolute right-4 bottom-[0.5px]"
+            className={`w-[148px] h-[5px] rounded-t-lg transition-all duration-300 ease bg-primary absolute ${
+              displaySection === 'products' ? 'right-4' : 'right-[200px]'
+            } bottom-[0.5px]`}
           ></div>
         </div>
 

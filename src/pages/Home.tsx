3278dca72d924cb4f865d77bf177b9bcedee45f3
Ali Bayat <ali.bayat77@yahoo.com>
2023-04-12 18:35:07 +0430

    raect loading spinner added to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,34 @@ import Navbar from '../components/common/Navbar'
 import Main from '../components/Landing ui/Main'
 import AuthenticatedHome from '../components/Landing ui/AuthenticatedHome/AuthenticatedHome'
 import { isUserLoggedIn } from '../hooks/isUserLoggedIn'
+import { useEffect, useState } from 'react'
+import { ColorRing } from 'react-loader-spinner'
 
 const Home = () => {
   const isLoggedIn = isUserLoggedIn()
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
 
   return (
     <>
-      {isLoggedIn ? (
+      {loading ? (
+        <div className="w-screen h-screen flex justify-center items-center">
+          <ColorRing
+            visible={true}
+            height="120"
+            width="120"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={['#0182FE', '#0182FE', '#60B1FE', '#DFEFFF', '#DFEFFF']}
+          />
+        </div>
+      ) : isLoggedIn ? (
         <AuthenticatedHome />
       ) : (
         <div className="flex flex-col justify-center items-center w-full">

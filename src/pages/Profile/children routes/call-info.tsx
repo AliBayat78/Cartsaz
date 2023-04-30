454b8daad368d6e65a87f560a62f75ad9643bab4
Ali Bayat <ali.bayat77@yahoo.com>
2023-04-30 19:57:54 +0430

    navbar call-info QR-code and vitrin-setting added

--- src/pages/Profile/children routes/call-info.tsx
+++ src/pages/Profile/children routes/call-info.tsx
@@ -1,5 +1,24 @@
+import { useNavigate } from 'react-router-dom'
+import arrowRight from '../assets/arrow-right.png'
+
 const CallInformation = () => {
-    return ( <div>call info</div> );
+  const navigate = useNavigate()
+
+  return (
+    <div className="flex flex-col items-center w-full h-auto">
+      <nav className="flex flex-row justify-end items-center w-full h-16 border border-b-silver p-2">
+        <div className="sm:mr-8 gap-4 flex flex-row justify-center items-center">
+          <p>اطلاعات تماس</p>
+          <img
+            onClick={() => navigate(-1)}
+            className="cursor-pointer"
+            src={arrowRight}
+            alt="back"
+          />
+        </div>
+      </nav>
+    </div>
+  )
 }
- 
-export default CallInformation;
+
+export default CallInformation

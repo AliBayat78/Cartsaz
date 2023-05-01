7b3bf8ff06731b505d105f975167c5bf8543d084
Ali Bayat <ali.bayat77@yahoo.com>
2023-05-01 05:27:02 +0430

    qr code page completed responsively

--- src/pages/Profile/children routes/QRcode.tsx
+++ src/pages/Profile/children routes/QRcode.tsx
@@ -1,8 +1,12 @@
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import arrowRight from '../assets/arrow-right.png'
+import QRCode from 'qrcode.react'
+import arrowDown from '../assets/arrow-down.png'
+import printer from '../assets/printer.png'
 
 const QRcode = () => {
   const navigate = useNavigate()
+  const url = useLocation()
 
   return (
     <div className="flex flex-col items-center w-full h-auto">
@@ -17,6 +21,35 @@ const QRcode = () => {
           />
         </div>
       </nav>
+
+      <main className="flex flex-col justify-center items-center mt-8 2xs:w-[260px] sm:w-[364px]">
+        <div>
+          <QRCode
+            value={`${window.location.origin}/${url.pathname}`}
+            size={128}
+            fgColor="#000000"
+            bgColor="#ffffff"
+            level="L"
+          />
+        </div>
+
+        <div className="mt-4 flex flex-col justify-center items-center w-full">
+          <button className="2xs:w-[260px] sm:w-[350px] h-[56px] bg-light-silver rounded-lg mt-4 flex flex-row justify-center items-center gap-3">
+            <p className="button-sm">پرینت مستقیم</p>
+            <img src={printer} alt="print" />
+          </button>
+          <button className="2xs:w-[260px] sm:w-[350px] h-[56px] bg-primary text-white rounded-lg mt-2 flex flex-row justify-center items-center gap-3">
+            <p className="button-sm text-white">دانلود تصویر</p>
+            <img src={arrowDown} alt="download" />
+          </button>
+
+          <p className="mt-4 body-xs">
+            این qur کد اختصاصی ویترین فروشگاه شماست که میتوانید آن را چاپ کرده و در مغازه یا فروشگاه
+            خود درج کنید تا کاربران به راحتی با اسکن این تصویر به ویترین شما بیایند و محصولات شما را
+            به صورت دیجیتالی مشاهده کنند.
+          </p>
+        </div>
+      </main>
     </div>
   )
 }

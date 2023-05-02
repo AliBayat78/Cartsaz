2d0ca0a2c7428b83229b062eb5d386cb7ed27fe7
Ali Bayat <ali.bayat77@yahoo.com>
2023-05-03 01:07:57 +0430

    html2canvas added, QRCode download button fixed

--- src/pages/Profile/children routes/QRcode.tsx
+++ src/pages/Profile/children routes/QRcode.tsx
@@ -3,12 +3,25 @@ import arrowRight from '../assets/arrow-right.png'
 import QRCode from 'qrcode.react'
 import arrowDown from '../assets/arrow-down.png'
 import printer from '../assets/printer.png'
+import { useRef } from 'react'
+import html2canvas from 'html2canvas'
 
 const QRcode = () => {
   const navigate = useNavigate()
+  const qrcodeRef = useRef<HTMLDivElement | null>(null)
 
   const localStorageUserValue = JSON.parse(localStorage.getItem('user') || 'null')
   const vitrinUrl = localStorageUserValue !== null ? localStorageUserValue.VitrinUrl : 'Error'
+
+  const handleScreenshot = () => {
+    html2canvas(qrcodeRef.current!).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png')
+      const downloadLink = document.createElement('a')
+      downloadLink.href = imgData
+      downloadLink.download = 'QRCode.png'
+      downloadLink.click()
+    })
+  }
 
   return (
     <div className="flex flex-col items-center w-full h-auto">
@@ -25,7 +38,7 @@ const QRcode = () => {
       </nav>
 
       <main className="flex flex-col justify-center items-center mt-8 2xs:w-[260px] sm:w-[364px]">
-        <div className="myDivToPrint">
+        <div ref={qrcodeRef} className="myDivToPrint">
           <QRCode value={vitrinUrl} size={128} fgColor="#000000" bgColor="#ffffff" level="L" />
         </div>
 
@@ -37,7 +50,10 @@ const QRcode = () => {
             <p className="button-sm">پرینت مستقیم</p>
             <img src={printer} alt="print" />
           </button>
-          <button className="2xs:w-[260px] sm:w-[350px] h-[56px] bg-primary text-white rounded-lg mt-2 flex flex-row justify-center items-center gap-3">
+          <button
+            onClick={() => handleScreenshot()}
+            className="2xs:w-[260px] sm:w-[350px] h-[56px] bg-primary text-white rounded-lg mt-2 flex flex-row justify-center items-center gap-3"
+          >
             <p className="button-sm text-white">دانلود تصویر</p>
             <img src={arrowDown} alt="download" />
           </button>
